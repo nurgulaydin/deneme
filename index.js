fbb44ddc144b2b5914e99508690585df9209e2dd
Nurgul Aydin <85848382+nurgulaydin@users.noreply.github.com>
2023-02-06 17:20:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,25 +18,39 @@ let kisiler = [
         maas: 6500
     }
 ]
-let $k1 = document.getElementById("kisi1")
-let $k2 = document.getElementById("kisi2")
-let $k3 = document.getElementById("kisi3")
+window.addEventListener("load", ()=>{
+    let $k1 = document.getElementById("kisi1")
+    let $k2 = document.getElementById("kisi2")
+    let $k3 = document.getElementById("kisi3")
+
+    function kisi1() {
+        $k1.innerHTML = "Ad: " + kisiler[0].ad + "</br>" + "Soyad:" + kisiler[0].soyad  
+         + "</br>" + "Yaş:" + kisiler[0].yas + "</br>" + "Maaş:" +  kisiler[0].maas
+     }
+     
+     function kisi2() {
+         $k2.innerHTML = "Ad: " + kisiler[1].ad + "</br>" + "Soyad:" + kisiler[1].soyad  
+         + "</br>" + "Yaş:" + kisiler[1].yas + "</br>" + "Maaş:" +  kisiler[1].maas
+     }
+     
+     function kisi3() {
+         $k3.innerHTML = "Ad: " + kisiler[2].ad + "</br>" + "Soyad:" + kisiler[2].soyad  
+         + "</br>" + "Yaş:" + kisiler[2].yas + "</br>" + "Maaş:" +  kisiler[2].maas
+     }
+     
+     
+    kisi1()
+    kisi2()
+    kisi3()
+
+    
 
 
-function kisi1() {
-   $k1.textContent = "Ad: " + kisiler[0].ad + "</br>" + "Soyad:" + kisiler[0].soyad  
-    + "</br>" + "Yaş:" + kisiler[0].yas + "</br>" + "Maaş:" +  kisiler[0].maas
-}
+})
 
-function kisi2() {
-    $k2.textContent = "Ad: " + kisiler[1].ad + "</br>" + "Soyad:" + kisiler[1].soyad  
-    + "</br>" + "Yaş:" + kisiler[1].yas + "</br>" + "Maaş:" +  kisiler[1].maas
-}
 
-function kisi3() {
-    $k3.textContent = "Ad: " + kisiler[2].ad + "</br>" + "Soyad:" + kisiler[2].soyad  
-    + "</br>" + "Yaş:" + kisiler[2].yas + "</br>" + "Maaş:" +  kisiler[2].maas
-}
+
+
 
 
 function maasHesapla() {
@@ -48,7 +62,3 @@ function yasHesapla() {
     let ort = (kisiler[0].yas + kisiler[1].yas + kisiler[2].yas) / 3;
     document.getElementById("yasSonuc").innerHTML = ort;
 }
-
-window.addEventListener("DOMContentLoaded", ()=> {kisi1()
-    kisi2()
-    kisi3()})
