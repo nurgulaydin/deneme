bb2d0cff3a1d6025288262b6329e6210935834f2
Nurgul Aydin <85848382+nurgulaydin@users.noreply.github.com>
2023-02-14 09:49:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,40 @@
 let kisiler = [
-    {
-        ad: "Nurgül",
-        soyad: "Aydın",
-        yas: 22,
-        maas: 5500
-    },
-    {
-        ad: "Selenay",
-        soyad: "Abana",
-        yas: 23,
-        maas: 6000
-    },
-    {
-        ad: "Ravil",
-        soyad: "Askarov",
-        yas: 21,
-        maas: 6500
-    }
-]
-window.addEventListener("load", ()=>{
-    let $k1 = document.getElementById("kisi1")
-    let $k2 = document.getElementById("kisi2")
-    let $k3 = document.getElementById("kisi3")
+  {
+    ad: "Nurgül",
+    soyad: "Aydın",
+    yas: 22,
+    maas: 5500,
+  },
+  {
+    ad: "Selenay",
+    soyad: "Abana",
+    yas: 23,
+    maas: 6000,
+  },
+  {
+    ad: "Ravil",
+    soyad: "Askarov",
+    yas: 21,
+    maas: 6500,
+  },
+];
+let ortYas = 0;
+let ortMaas = 0;
 
-    function kisi1() {
-        $k1.innerHTML = "Ad: " + kisiler[0].ad + "</br>" + "Soyad:" + kisiler[0].soyad  
-         + "</br>" + "Yaş:" + kisiler[0].yas + "</br>" + "Maaş:" +  kisiler[0].maas
-     }
-     
-     function kisi2() {
-         $k2.innerHTML = "Ad: " + kisiler[1].ad + "</br>" + "Soyad:" + kisiler[1].soyad  
-         + "</br>" + "Yaş:" + kisiler[1].yas + "</br>" + "Maaş:" +  kisiler[1].maas
-     }
-     
-     function kisi3() {
-         $k3.innerHTML = "Ad: " + kisiler[2].ad + "</br>" + "Soyad:" + kisiler[2].soyad  
-         + "</br>" + "Yaş:" + kisiler[2].yas + "</br>" + "Maaş:" +  kisiler[2].maas
-     }
-     
-     
-    kisi1()
-    kisi2()
-    kisi3()
-
-    
-
-
-})
-
-
-
-
-
+window.addEventListener("load", () => {
+  kisiler.forEach((value, index) => {
+    document.getElementById("kisi" + index).innerHTML = "Ad: " + value.ad + "</br>" + "Soyad:" + value.soyad + "</br>" + "Yaş:" + value.yas + "</br>" + "Maaş:" + value.maas;
+    ortMaas += value.maas;
+    ortYas += value.yas;
+  });
+  ortMaas = ortMaas / 3;
+  ortYas = ortYas / 3;
+});
 
 function maasHesapla() {
-    let ort = (kisiler[0].maas + kisiler[1].maas + kisiler[2].maas) / 3
-    document.getElementById("maasSonuc").innerHTML = ort;
+  document.getElementById("maasSonuc").innerHTML = ortMaas;
 }
 
 function yasHesapla() {
-    let ort = (kisiler[0].yas + kisiler[1].yas + kisiler[2].yas) / 3;
-    document.getElementById("yasSonuc").innerHTML = ort;
+  document.getElementById("yasSonuc").innerHTML = ortYas;
 }
